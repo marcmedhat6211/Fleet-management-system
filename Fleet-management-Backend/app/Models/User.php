f231fac427b331dbd7b40afd19bb0ba559ef2abf
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-12-21 17:25:02 +0200

    hashing the password

--- Fleet-management-Backend/app/Models/User.php
+++ Fleet-management-Backend/app/Models/User.php
@@ -56,4 +56,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function setPasswordAttribute($value) {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
