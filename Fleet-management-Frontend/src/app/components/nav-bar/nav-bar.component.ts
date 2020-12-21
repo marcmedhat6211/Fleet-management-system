fbf8a52f7bd060a9293e26f66a8a57e815a23f34
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-12-21 20:18:48 +0200

    handling what appears on navbar when logging in or logging out

--- Fleet-management-Frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ Fleet-management-Frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,8 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStatusService } from 'src/app/services/auth-status.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  public loggedIn: boolean;
+
+  constructor(
+    private authStatus: AuthStatusService,
+    private router: Router,
+    private token: TokenService
+  ) { }
 
   ngOnInit(): void {
+    this.authStatus.authStatus.subscribe(value => this.loggedIn = value);
+  }
+
+  logout(event: MouseEvent) {
+    event.preventDefault();
+    this.token.remove();
+    this.authStatus.changeAuthStatus(false);
+    this.router.navigateByUrl('/');
   }
 
 }
