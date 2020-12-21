3ad6ee3dfa0f271f93d685da43fe654017f56a25
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-12-21 12:13:41 +0200

    setting the login form and configurations for frontend

--- Fleet-management-Frontend/src/app/components/login/login.component.ts
+++ Fleet-management-Frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form = {
+    email: null,
+    password: null,
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  onSubmit() {
+    return this.httpClient.post('http://localhost:8000/api/auth/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)      
+    );
+  }
 
   ngOnInit(): void {
   }
